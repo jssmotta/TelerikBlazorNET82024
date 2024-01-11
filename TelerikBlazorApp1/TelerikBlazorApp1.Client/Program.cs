77c4f912ecdf92973d1215bfa19383657dbfd619
Jefferson S. Motta <55891584+jssmotta@users.noreply.github.com>
2024-01-10 23:15:49 -0300

    Main Version

---
+++ TelerikBlazorApp1/TelerikBlazorApp1.Client/Program.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddTelerikBlazor();
+ 
+
+await builder.Build().RunAsync();
